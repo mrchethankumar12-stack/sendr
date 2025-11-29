3e184a2964cf0e59f0a1ce26539b6f385795c43b
ChethanKumarDeveloper <chethan22ec011@drttit.edu.in>
2025-11-29 07:43:48 +0530

    Update productService.js

--- public/src/services/productService.js
+++ public/src/services/productService.js
@@ -1,1 +1,16 @@
+// src/services/productService.js
+import { collection, addDoc, doc, updateDoc, deleteDoc } from "firebase/firestore";
+import { db } from "../firebase";
+
+export async function addProduct(data) {
+  return await addDoc(collection(db, "products"), data);
+}
+
+export async function updateProduct(id, data) {
+  return await updateDoc(doc(db, "products", id), data);
+}
+
+export async function deleteProduct(id) {
+  return await deleteDoc(doc(db, "products", id));
+}
 
